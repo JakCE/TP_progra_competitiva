b7e8df809de0d8d647f6deb9e27f8b3a7cac893a
Jak Campos <103612662+JakCE@users.noreply.github.com>
2023-05-02 11:32:47 -0500

    Add files via upload

--- Map_STL_TP.cpp
+++ Map_STL_TP.cpp
@@ -4,3 +4,24 @@ Jak Cristian Campos Espinoza -U20211A184
 Alejandro Olaf López Flores - U201922773
 */
 
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+void contar_palabras(const vector<string>& palabras) {
+    map<string, int> frecuencia;
+    for (const string& word : palabras) {
+        frecuencia[word]++;
+    }
+    for (auto entry : frecuencia) {
+        cout << entry.first << " " << entry.second << endl;
+    }
+}
+
+int main() {
+    vector<string> palabras = { "gato", "perro", "gato", "ratón", "perro", "perro" };
+    contar_palabras(palabras);
+    return 0;
+}
